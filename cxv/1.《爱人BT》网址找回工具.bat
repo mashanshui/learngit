c10480af071e2e2e8e6d6acb909e2602f9254ef0
mashanshui <911587154@qq.com>
2019-06-30 18:14:29 +0800

    你懂的

---
+++ cxv/1.《爱人BT》网址找回工具.bat
@@ -0,0 +1,5 @@
+%%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a 
+cls
+@echo off&color 0a&for %%a in (dns-chinaa,dns-chinab,dns-chinac) do call :jx %%a
+:jx %1
+ping www.%1.com&&for /f "tokens=2 delims=[]" %%b in ('ping -n 1 www.%1.com^|findstr "["') do explorer http://%%b/&exit 
